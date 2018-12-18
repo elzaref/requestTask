f93c006a623681593b90fa7766a1697460ea3ce9
mostafa.elzaref <mostafa.elzaref@gmail.com>
2018-12-18 20:38:37 +0200

    Add project files.

---
+++ RequestApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace RequestApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
